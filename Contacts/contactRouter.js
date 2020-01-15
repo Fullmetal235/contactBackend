e1d88f51eaea5ea7cebee0f74d3ce49510a46be8
Fullmetal235 <jjbillupsbrown@gmail.com>
2020-01-14 21:43:30 -0600

    fixing access-control-origin

--- Contacts/contactRouter.js
+++ Contacts/contactRouter.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 const db = require("./contactModel");
 
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 //GET Request
 router.get("/", (req, res) => {
   db.find()
