96107121a8875220a50343c6d12c321d277d1e1d
Fullmetal235 <jjbillupsbrown@gmail.com>
2020-01-15 04:25:26 +0000

    switching cors

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const contactRouter = require("./Contacts/contactRouter.js");
 const cors = require('cors');
 const server = express();
 server.use(express.json());
+server.use(cors());
 server.use("/contacts", contactRouter);
 //server.use('/locations', locationRouter)
-server.use(cors());
 server.use("/", (req, res) => {
   res.send("GRIIIIFFFITTH!!!!!!!!");
 });
