9df558b7a90d1b5269b8a2f3fc9c04bfc66b6e1b
Fullmetal235 <jjbillupsbrown@gmail.com>
2020-01-16 00:12:51 +0000

    fixing dbconfig

--- knexfile.js
+++ knexfile.js
@@ -16,7 +16,7 @@ module.exports = {
       client: "pg",
       connection: process.env.DATABASE_URL,
       migrations: {
-        directory: "./migrations"
+        directory: "./data/migrations"
       },
       seeds: {
         directory: "./seeds"
