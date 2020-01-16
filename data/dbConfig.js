680214fa72673ec0288ea1c4748f5b994c32e5d3
Fullmetal235 <jjbillupsbrown@gmail.com>
2020-01-16 01:43:49 +0000

    Fixing config

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,5 +1,5 @@
-const knex = require("knex");
+const knex = require('knex');
 
-const knexConfig = require("../knexfile.js");
-const dbENV = process.env.db_ENV || "development";
-module.exports = knex(knexConfig.development);
+const knexConfig = require('../knexfile.js');
+const dbENV = process.env.db_ENV || 'development';
+module.exports = knex(knexConfig[dbENV]);
