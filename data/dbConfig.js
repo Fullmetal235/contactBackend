37664f026547574ce40feba41150e9ae8a64e1a2
Fullmetal235 <jjbillupsbrown@gmail.com>
2020-09-06 14:58:08 -0500

    did an ole switcheroo

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,6 +1,5 @@
-const knex = require('knex');
+const knex = require("knex");
 
-const knexConfig = require('../knexfile.js');
-const dbENV = process.env.db_ENV || 'development';
-module.exports = knex(knexConfig[dbENV]);
-
+const knexConfig = require("../knexfile.js");
+const dbENV = process.env.db_ENV || "development";
+module.exports = knex(knexConfig.production);
